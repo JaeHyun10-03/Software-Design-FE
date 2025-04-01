c1fd77a756df4eca4fbff0874855aa62a42375ac
LimJaeHyun <guswogus81@gmail.com>
2025-04-01 10:07:17 +0900

    ⬆️ update : 헤더 페이지 라우터 적용

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,10 +1,19 @@
 import React, { ReactNode } from "react";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import logo from "@/assets/images/logo.png";
 
 export const Header = ({ children }: { children: ReactNode }) => {
+  const router = useRouter();
+
   // 헤더 내 페이지 데이터
-  const menuItems = ["학적", "성적", "상담", "출결", "행동"];
+  const menuItems = [
+    { label: "학적", path: "/student-record" },
+    { label: "성적", path: "/grade" },
+    { label: "상담", path: "/counsel" },
+    { label: "출결", path: "/attendance" },
+    { label: "행동", path: "/behavior" },
+  ];
 
   return (
     <>
@@ -12,8 +21,8 @@ export const Header = ({ children }: { children: ReactNode }) => {
         <Image src={logo} alt="로고" height={64} />
         <div className="flex items-center gap-16 ml-4">
           {menuItems.map((item, index) => (
-            <div key={index} className="font-bold text-xl text-center text-[#333333] cursor-pointer">
-              {item}
+            <div key={index} className="font-bold text-xl text-center text-[#333333] cursor-pointer" onClick={() => router.push(item.path)}>
+              {item.label}
             </div>
           ))}
         </div>
