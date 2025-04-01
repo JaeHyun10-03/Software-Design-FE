3fd0275412f38926f7f4fea5473c0f70b5f047c7
LimJaeHyun <guswogus81@gmail.com>
2025-04-01 15:01:35 +0900

    ✨ feat: 카테고리 선택 기능 구현

--- src/components/student-record/Category.tsx
+++ src/components/student-record/Category.tsx
@@ -1,19 +1,24 @@
-import React, { ReactNode } from "react";
+import useCategoryStore from "@/store/category-store";
+import React from "react";
 
-export const Category = ({ children }: { children: ReactNode }) => {
-  const categories = ["학적", "성적", "특기사항", "출결", "행동", "상담"];
+export const Category = () => {
+  const categoryList = ["학적", "성적", "특기사항", "출결", "행동", "상담"];
+  const { category, setCategory } = useCategoryStore();
   return (
     <>
       <div className="w-full h-8">
         <div className="flex w-full h-full">
-          {categories.map((category, index) => (
-            <div key={category} className={`flex-1 flex items-center justify-center py-1 border border-solid border-[#a9a9a9] ${index === 0 ? "bg-[#4daaf880]" : ""}`}>
-              <div className="font-normal text-[#333333] text-base text-center font-['Nanum_Gothic-Regular',Helvetica]">{category}</div>
+          {categoryList.map((categoryData, index) => (
+            <div
+              key={index}
+              className={`flex-1 flex items-center justify-center py-1 border border-solid border-[#a9a9a9] cursor-pointer ${category === categoryData ? "bg-[#4DAAF880]" : ""}`}
+              onClick={() => setCategory(categoryData)}
+            >
+              <div className="font-normal text-[#333333] text-base text-center font-['Nanum_Gothic-Regular',Helvetica]">{categoryData}</div>
             </div>
           ))}
         </div>
       </div>
-      {children}
     </>
   );
 };
---
+++ src/components/student-record/Content.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Category } from "@/components/student-record/Category";
+
+export default function Content() {
+  return (
+    <div className="w-screen h-screen border  border-gray-400">
+      <Category />
+    </div>
+  );
+}
