853327de7b1f9544f54fd4c6300cb4b3956dc19e
LimJaeHyun <guswogus81@gmail.com>
2025-04-01 14:44:07 +0900

    ♻️ refactor: 성능 최적화 & 호버, 포커스 적용

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,33 +1,53 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useMemo, useCallback } from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import logo from "@/assets/images/logo.png";
 
 export const Header = ({ children }: { children: ReactNode }) => {
   const router = useRouter();
+  const menuItems = useMemo(
+    () => [
+      { label: "학적", path: "/student-record" },
+      { label: "성적", path: "/grade" },
+      { label: "상담", path: "/counsel" },
+      { label: "출결", path: "/attendance" },
+      { label: "행동", path: "/behavior" },
+    ],
+    []
+  );
 
-  // 헤더 내 페이지 데이터
-  const menuItems = [
-    { label: "학적", path: "/student-record" },
-    { label: "성적", path: "/grade" },
-    { label: "상담", path: "/counsel" },
-    { label: "출결", path: "/attendance" },
-    { label: "행동", path: "/behavior" },
-  ];
+  const handleNavigation = useCallback(
+    (path: string) => {
+      router.push(path);
+    },
+    [router]
+  );
+
+  const menuElements = useMemo(
+    () =>
+      menuItems.map((item, index) => (
+        <nav
+          key={index}
+          className={`
+          font-bold text-xl text-center cursor-pointer
+          ${router.pathname === item.path ? "text-[#0064FF]" : "text-[#333333]"}
+          hover:text-[#0057E6] active:text-[#0064FF]
+        `}
+          onClick={() => handleNavigation(item.path)}
+        >
+          {item.label}
+        </nav>
+      )),
+    [menuItems, router.pathname, handleNavigation]
+  );
 
   return (
-    <>
+    <header>
       <div className="flex items-center w-full h-18 bg-white border-b border-gray-400">
         <Image src={logo} alt="로고" height={64} />
-        <div className="flex items-center gap-16 ml-4">
-          {menuItems.map((item, index) => (
-            <div key={index} className="font-bold text-xl text-center text-[#333333] cursor-pointer" onClick={() => router.push(item.path)}>
-              {item.label}
-            </div>
-          ))}
-        </div>
+        <div className="flex items-center gap-16 ml-4">{menuElements}</div>
       </div>
       {children}
-    </>
+    </header>
   );
 };
