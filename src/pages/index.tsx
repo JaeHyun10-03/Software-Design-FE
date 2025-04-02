abe56c1acfa0034bf279f14bcc83ac8a9bfa57f2
yereong <lily030624@gmail.com>
2025-04-02 19:18:29 +0900

    [NEEIS-27] ✨ feat: 로그인 마크업 반응형 처리

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,22 +20,22 @@ const Login = () => {
   };
 
   return (
-    <div >
-      <div className="flex w-full h-full flex-col items-center jsutify-start">
-        <Image className="mt-[3%]" src={"/assets/images/logo.svg"} alt="logo" width={380} height={190}></Image>
-        <div id="inputCont" className="flex flex-col align-start mt-[32px]">
+    <div className="flex justify-center items-center" >
+      <div className="flex w-full h-full max-w-[500px] flex-col items-center jsutify-start">
+        <Image className="mt-[13%]" src={"/assets/images/logo.svg"} alt="logo" width={380} height={190}></Image>
+        <div id="inputCont" className="flex flex-col align-start mt-[32px] w-[76%]">
           <div id="id" className="mb-[32px]">
             <p className="mb-[8px] text-[20px] leading-[23px] text-center text-[#333333] flex items-center font-[NanumGothic]" >아이디</p>
-            <input className="flex w-[380px] h-[56px] border border-[#A9A9A9] rounded-[10px]"></input>
+            <input className="flex w-[100%] h-[56px] border border-[#A9A9A9] rounded-[10px]"></input>
           </div>
           <div id="password" className="  mb-[64px]">
             <p className="mb-[8px] text-[20px] leading-[23px] text-center text-[#333333] flex items-center font-[NanumGothic]">비밀번호</p>
-            <input type="password" className="border w-[380px] h-[56px] border-[#A9A9A9] rounded-[10px] px-[3px]"></input>
+            <input type="password" className="border w-[100%] h-[56px] border-[#A9A9A9] rounded-[10px] px-[3px]"></input>
           </div>
 
-          <button className="flex justify-center items-center w-[380px] h-[56px] bg-[#0064FF] rounded-[10px] text-white font-[NanumGothic] font-semibold text-[20px] leading-[23px] mb-[32px] ">로그인하기</button>
+          <button className="flex justify-center items-center w-[100%] h-[56px] bg-[#0064FF] rounded-[10px] text-white font-[NanumGothic] font-semibold text-[20px] leading-[23px] mb-[32px] ">로그인하기</button>
 
-          <div id="findCont" className="flex flex-row w-[380px] justify-around font-[NanumGothic] text-[#A9A9A9] font-normal text-[20px] leading-[23px]">
+          <div id="findCont" className="flex flex-row w-[100%] justify-around font-[NanumGothic] text-[#A9A9A9] font-normal text-[20px] leading-[23px]">
             <div>아이디 찾기</div>
             |
             <div>비밀번호 찾기</div>
