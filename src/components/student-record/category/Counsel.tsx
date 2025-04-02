ee37de55711947095537691ee562789300f83ab2
LimJaeHyun <guswogus81@gmail.com>
2025-04-02 21:27:02 +0900

    ♻️ refactor: 학적 내 컨텐츠 컴포넌트

---
+++ src/components/student-record/category/Counsel.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Counsel() {
+  return <div>Counsel</div>;
+}
---
+++ src/components/student-record/category/Grade.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Grade() {
+  return <div>성적 카테고리입니다</div>;
+}
