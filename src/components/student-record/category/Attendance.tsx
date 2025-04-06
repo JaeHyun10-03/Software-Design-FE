aaba6af017890b851a57c665ca65f0a76fde5480
LimJaeHyun <guswogus81@gmail.com>
2025-04-07 00:39:16 +0900

    ✨ feat: 출결 퍼블리싱

--- src/components/student-record/category/Attendance.tsx
+++ src/components/student-record/category/Attendance.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import Cell from "../Cell";
+import AttendanceCalendar from "../AttendanceCalendar";
+
+const attendanceHeaders = ["수업일수", "출석", "조퇴", "지각", "결석"];
 
 export default function Attendance() {
-  return <div>출결</div>;
+  return (
+    <div>
+      <div className="flex">
+        {attendanceHeaders.map((a, index) => {
+          return <Cell key={index}>{a}</Cell>;
+        })}
+      </div>
+      <AttendanceCalendar></AttendanceCalendar>
+    </div>
+  );
 }
