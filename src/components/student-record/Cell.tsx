499d995fdb1804aa67721213ba1538384df885b9
LimJaeHyun <guswogus81@gmail.com>
2025-04-06 22:32:45 +0900

    ✨ feat: 셀 컴포넌트 추가

---
+++ src/components/student-record/Cell.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from "react";
+
+interface CellProps {
+  children: ReactNode;
+  onClick?: () => void;
+}
+
+export default function Cell({ children, onClick }: CellProps) {
+  const baseClass = "flex flex-1 items-center justify-center h-8 border border-gray-400";
+  const clickableClass = onClick ? "cursor-pointer" : "";
+
+  return (
+    <div className={`${baseClass} ${clickableClass}`} onClick={onClick}>
+      <p className="text-base text-center text-gray-800">{children}</p>
+    </div>
+  );
+}
