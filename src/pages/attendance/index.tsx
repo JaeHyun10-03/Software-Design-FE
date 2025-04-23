c738470d9c823438c2843a8fb66acfce0af38cf2
LimJaeHyun <guswogus81@gmail.com>
2025-04-23 20:57:52 +0900

    ⬆️ update : 연도 필터 & 출결 타입 표시

--- src/pages/attendance/index.tsx
+++ src/pages/attendance/index.tsx
@@ -1,4 +1,6 @@
 import AttendanceContent from "@/components/attendance/attendanceContent";
+import DateFilter from "@/components/attendance/DateFilter";
+import AttendanceType from "@/components/shared/AttendanceType";
 import Button from "@/components/shared/Button";
 import { Header } from "@/components/shared/Header";
 import StudentFilter from "@/components/shared/StudentFilter";
@@ -7,9 +9,14 @@ import React, { ReactNode } from "react";
 export default function AttendancePage() {
   return (
     <div className="mx-8 mt-4 mb-8 h-[calc(100vh-120px)] flex flex-col">
-      <div className="flex h-8">
+      <div className="flex h-8 gap-4">
         <StudentFilter />
-        <div className="ml-auto">
+        <DateFilter />
+
+        <div className="flex flex-row gap-4 ml-auto">
+          <div className="hidden lg:flex">
+            <AttendanceType />
+          </div>
           <Button className="w-20 h-8">저장</Button>
         </div>
       </div>
