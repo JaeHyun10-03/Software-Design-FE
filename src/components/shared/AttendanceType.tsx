efb636e111a6aca71e21fb261b9ef5268ac9d029
LimJaeHyun <guswogus81@gmail.com>
2025-04-23 20:57:12 +0900

    ✨ feat: 출결 타입 표시

---
+++ src/components/shared/AttendanceType.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AttendanceType = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <p className="flex text-base text-right text-black ">O : 출석 | △ : 조퇴 | ▲ : 지각 | X : 결석</p>
+    </div>
+  );
+};
+
+export default AttendanceType;
