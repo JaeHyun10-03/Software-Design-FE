ebb9fa5a214d909fd76468cecb6fa260a070d28d
LimJaeHyun <guswogus81@gmail.com>
2025-04-24 16:12:28 +0900

    ✨ feat: 행동 페이지 마크업

---
+++ src/components/behavior/BehaviorContent.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Button from "../shared/Button";
+
+const BehaviorContent = () => {
+  return (
+    <div className="w-full h-full border border-[#a9a9a9] p-4">
+      <div className="flex flex-col gap-2">
+        <p className="text-xl">행동특성</p>
+        <textarea className="min-h-48 border border-[#a9a9a9] rounded-md text-[#333333] text-base resize-none p-4 focus:outline-none"></textarea>
+        <Button className="w-16 h-8 ml-auto">저장</Button>
+      </div>
+      <div className="flex flex-col gap-2 mt-16">
+        <p className="text-xl">종합의견</p>
+        <textarea className="min-h-48 border border-[#a9a9a9] rounded-md text-[#333333] text-base resize-none p-4 focus:outline-none"></textarea>
+        <Button className="w-16 h-8 ml-auto">저장</Button>
+      </div>
+    </div>
+  );
+};
+
+export default BehaviorContent;
--- src/pages/behavior/index.tsx
+++ src/pages/behavior/index.tsx
@@ -1,8 +1,21 @@
+import BehaviorContent from "@/components/behavior/BehaviorContent";
 import { Header } from "@/components/shared/Header";
-import React, { ReactNode, useState } from "react";
+import StudentFilter from "@/components/shared/StudentFilter";
+import StudentList from "@/components/shared/StudentList";
+import React, { ReactNode } from "react";
 
 export default function BehaviorPage() {
-  return <div>행동 페이지입니다</div>;
+  return (
+    <div className="mx-8 mt-4 mb-8 h-[calc(100vh-120px)] flex flex-col">
+      <div className="flex h-8">
+        <StudentFilter />
+      </div>
+      <div className="flex flex-row gap-8 mt-4 flex-1">
+        <StudentList />
+        <BehaviorContent />
+      </div>
+    </div>
+  );
 }
 
 BehaviorPage.getLayout = (page: ReactNode) => {
