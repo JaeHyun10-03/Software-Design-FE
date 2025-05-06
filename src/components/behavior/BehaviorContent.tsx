a7bbf7865f73643138326c4f6bb52aa1d42bd0cb
LimJaeHyun <guswogus81@gmail.com>
2025-05-07 00:05:58 +0900

    🐛 fix: 버그 수정

--- src/components/behavior/BehaviorContent.tsx
+++ src/components/behavior/BehaviorContent.tsx
@@ -64,7 +64,7 @@ const BehaviorContent = () => {
     }
     try {
       const token = localStorage.getItem("accessToken");
-      const res = await axios.post(
+      const res = await axios.put(
         `${process.env.NEXT_PUBLIC_BACKEND_DOMAIN}/behavior?year=${year}&grade=${grade}&classNum=${classNumber}&studentId=${studentId}`,
         {
           behavior: behavior,
