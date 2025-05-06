0d5b642ed622c09042ce75c7ef0a1e2704c6dde6
LimJaeHyun <guswogus81@gmail.com>
2025-05-06 13:37:11 +0900

    ⬆️ update : 날짜 선택 컴포넌트 학기 추가

--- src/components/shared/DateFilter.tsx
+++ src/components/shared/DateFilter.tsx
@@ -10,6 +10,7 @@ interface SelectInputProps {
 
 export default function DateFilter(): React.ReactElement {
   const currentYear = new Date().getFullYear();
+  const currentMonth = new Date().getMonth() + 1; // getMonth()는 0부터 시작하므로 +1 필요
 
   const yearOptions: number[] = useMemo(() => {
     return Array.from({ length: 5 }, (_, i) => currentYear - 2 + i);
@@ -19,19 +20,33 @@ export default function DateFilter(): React.ReactElement {
     return Array.from({ length: 12 }, (_, i) => i + 1);
   }, []);
 
-  const { year, month, setYear, setMonth } = useSelectedDate();
+  const { year, month, semester, setYear, setMonth, setSemester } = useSelectedDate();
 
-  const handleYearChange = useCallback((event: React.ChangeEvent<HTMLSelectElement>) => {
-    setYear(Number(event.target.value));
-  }, []);
+  // 초기 값이 없는 경우, 현재 연도와 월을 디폴트로 설정
+  const handleYearChange = useCallback(
+    (event: React.ChangeEvent<HTMLSelectElement>) => {
+      setYear(Number(event.target.value));
+    },
+    [setYear]
+  );
 
-  const handleMonthChange = useCallback((event: React.ChangeEvent<HTMLSelectElement>) => {
-    setMonth(Number(event.target.value));
-  }, []);
+  const handleMonthChange = useCallback(
+    (event: React.ChangeEvent<HTMLSelectElement>) => {
+      setMonth(Number(event.target.value));
+    },
+    [setMonth]
+  );
+
+  const handleSemesterChange = useCallback(
+    (event: React.ChangeEvent<HTMLSelectElement>) => {
+      setSemester(Number(event.target.value));
+    },
+    [setSemester]
+  );
 
   const SelectInput = useCallback(
     ({ value, onChange, options, label }: SelectInputProps) => (
-      <div className="flex items-center gap-1 w-20">
+      <div className="flex items-center gap-1 w-21">
         <select className="w-20 h-6 border border-gray-400 text-gray-800 text-center text-base rounded" value={value} onChange={onChange}>
           {options.map((option) => (
             <option key={option} value={option}>
@@ -47,8 +62,9 @@ export default function DateFilter(): React.ReactElement {
 
   return (
     <div className="flex items-center gap-3 ml-4">
-      <SelectInput value={year} onChange={handleYearChange} options={yearOptions} label="년" />
-      <SelectInput value={month} onChange={handleMonthChange} options={monthOptions} label="월" />
+      <SelectInput value={year || currentYear} onChange={handleYearChange} options={yearOptions} label="년" />
+      <SelectInput value={month || currentMonth} onChange={handleMonthChange} options={monthOptions} label="월" />
+      <SelectInput value={semester} onChange={handleSemesterChange} options={[1, 2]} label="학기" />
     </div>
   );
 }
