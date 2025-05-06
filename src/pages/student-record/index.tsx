b89f470aa3f10e1db25948c20e921bf378482fe1
LimJaeHyun <guswogus81@gmail.com>
2025-05-06 13:37:41 +0900

    ⬆️ update : 출석, 결석, 지각, 조퇴 UI 추가

--- src/pages/student-record/index.tsx
+++ src/pages/student-record/index.tsx
@@ -13,7 +13,25 @@ export default function StudentRecordPage() {
     <div className="mx-8 mt-4 mb-8 h-[calc(100vh-120px)] flex flex-col">
       <div className="flex h-8">
         <StudentFilter />
-        {category === "출결" && <DateFilter></DateFilter>}
+        {category === "출결" && (
+          <div className="flex flex-row w-full">
+            <DateFilter></DateFilter>{" "}
+            <div className="flex ml-auto justify-center items-center attendance-legend gap-4 flex-wrap text-sm">
+              <div className="flex items-center gap-1">
+                <span className="font-bold">O</span> <span>출석</span>
+              </div>
+              <div className="flex items-center gap-1">
+                <span className="font-bold">X</span> <span>결석</span>
+              </div>
+              <div className="flex items-center gap-1">
+                <span className="font-bold">▲</span> <span>지각</span>
+              </div>
+              <div className="flex items-center gap-1">
+                <span className="font-bold">△</span> <span>조퇴</span>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
       <div className="flex flex-row gap-8 mt-4 flex-1">
         <StudentList />
