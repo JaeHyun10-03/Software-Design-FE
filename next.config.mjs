9ead0c4ec675128509494ce8dd25f66fcfbe5b0f
LimJaeHyun <guswogus81@gmail.com>
2025-05-20 22:32:08 +0900

    ⬆️ update : 학적 카테고리 사진 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    domains: [process.env.NEXT_PUBLIC_BACKEND_DOMAIN], // 실제 도메인으로 교체
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
