3141ef168cd4ea7c7cbad081880258ff4252b5ad
JaeHyun10-03 <guswogus81@gmail.com>
2025-05-25 19:24:45 +0000

    feat: 로그인 시 유저롤에 따라 리디렉션

---
+++ src/components/behavior/StudentBehaviorContent.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import useStudentFilterStore from "@/store/student-filter-store";
+import useSelectedDate from "@/store/selected-date-store";
+import { GetBehavior } from "@/api/student/getBehavior";
+const BehaviorContent = () => {
+  const { grade, classNumber, studentId } = useStudentFilterStore();
+  const { year } = useSelectedDate();
+  const [behavior, setBehavior] = useState("");
+  const [generalOpinion, setGeneralOpinion] = useState("");
+  // console.log(behaviorId);
+  // console.log(behavior);
+  // console.log(generalOpinion);
+
+  useEffect(() => {
+    const getBehavior = async () => {
+      try {
+        const res = await GetBehavior(year,  Number(grade),  Number(classNumber), Number(studentId));
+        const data = res.data.response;
+        setBehavior(data.behavior);
+        setGeneralOpinion(data.generalComment);
+        console.log("행동 데이터:", data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getBehavior();
+  }, [year, grade, classNumber, studentId]);
+
+ 
+  return (
+    <div className="w-full h-full border border-[#a9a9a9] p-4">
+      <div className="flex flex-col gap-2">
+        <p className="text-xl">행동특성</p>
+        <textarea
+          readOnly
+          value={behavior}
+          className="min-h-48 border border-[#a9a9a9] rounded-md text-[#333333] text-base resize-none p-4 focus:outline-none"
+        />
+      </div>
+      <div className="flex flex-col gap-2 mt-16">
+        <p className="text-xl">종합의견</p>
+        <textarea
+          readOnly
+          value={generalOpinion}
+          className="min-h-48 border border-[#a9a9a9] rounded-md text-[#333333] text-base resize-none p-4 focus:outline-none"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default BehaviorContent;
---
+++ src/components/shared/StudentHeader.tsx
@@ -0,0 +1,98 @@
+import React, { ReactNode, useMemo, useCallback, useState, useEffect } from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import AlertIcon from "@/assets/icons/AlertIcon";
+// import useStudentFilterStore from "@/store/student-filter-store";
+
+export const Header = ({ children }: { children: ReactNode }) => {
+  const router = useRouter();
+  // const { studentId } = useStudentFilterStore();
+  const [windowWidth, setWindowWidth] = useState(typeof window !== "undefined" ? window.innerWidth : 0);
+
+  // 모바일 화면인지 확인
+  const isMobile = windowWidth <= 600;
+
+  useEffect(() => {
+    // 윈도우 너비 변경을 감지하는 함수
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    // 컴포넌트 마운트 시 윈도우 너비 초기화
+    if (typeof window !== "undefined") {
+      setWindowWidth(window.innerWidth);
+      window.addEventListener("resize", handleResize);
+    }
+
+    // 컴포넌트 언마운트 시 이벤트 리스너 제거
+    return () => {
+      if (typeof window !== "undefined") {
+        window.removeEventListener("resize", handleResize);
+      }
+    };
+  }, []);
+
+  const menuItems = useMemo(
+    () => [
+      { label: "학적", path: "/student/student-record" },
+      { label: "성적", path: "/student//grade" },
+      { label: "상담", path: "/student//counsel" },
+      { label: "출결", path: "/student//attendance" },
+      { label: "행동", path: "/student//behavior" },
+    ],
+    []
+  );
+
+  const handleNavigation = useCallback(
+    (path: string) => {
+      router.push(path);
+    },
+    [router]
+  );
+
+  const menuElements = useMemo(
+    () =>
+      menuItems.map((item, index) => (
+        <nav
+          key={index}
+          className={`
+          font-bold text-center cursor-pointer
+          ${isMobile ? "text-base" : "text-xl"}
+          ${router.pathname === item.path ? "text-[#0064FF]" : "text-[#333333]"}
+          hover:text-[#0057E6] active:text-[#0064FF]
+        `}
+          onClick={() => handleNavigation(item.path)}
+        >
+          {item.label}
+        </nav>
+      )),
+    [menuItems, router.pathname, handleNavigation, isMobile]
+  );
+
+  return (
+    <header>
+      <div className="flex items-center w-full bg-white border-b border-gray-400">
+        <Image
+          src={"/assets/images/logo.png"}
+          alt="로고"
+          width={isMobile ? 45 : 71}
+          height={isMobile ? 30 : 48}
+          onClick={() => {
+            router.push("/");
+          }}
+          className={`cursor-pointer my-auto ${isMobile ? "mx-6" : "mx-8"}`}
+        />
+        <div className={`flex items-center h-[72px] ${isMobile ? "gap-8" : "gap-16"} ${isMobile ? "ml-1" : "ml-4"}`}>{menuElements}</div>
+        <div
+          className={`flex justify-center items-center ml-auto ${isMobile ? "mr-2" : "mr-8"}`}
+          onClick={() => {
+            router.push("/alert");
+          }}
+        >
+          <AlertIcon />
+        </div>
+      </div>
+      {children}
+    </header>
+  );
+};
