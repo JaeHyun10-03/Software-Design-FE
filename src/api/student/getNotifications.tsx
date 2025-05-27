583e066ecd68bcc7647e0d5090c75e993a870336
JaeHyun10-03 <guswogus81@gmail.com>
2025-05-27 17:15:56 +0000

    ✨feat: 알림 내역 가져오기 구현

---
+++ src/api/student/getNotifications.tsx
@@ -0,0 +1,33 @@
+import axios, { AxiosResponse } from "axios";
+
+
+export const GetNotification = async (
+    page: number,
+    size: number
+): Promise<any> => {
+  axios.defaults.withCredentials = true;
+  const accessToken = localStorage.getItem("accessToken");
+
+  try {
+    const response: AxiosResponse<any> = await axios.get(
+        `${process.env.NEXT_PUBLIC_BACKEND_DOMAIN}/notifications?page=${page}&size=${size}`,{
+          headers: { Authorization: `Bearer ${accessToken}` }
+        });
+
+    console.log(response.data.response);
+   
+      return response.data.response;
+  } catch (error: any) {
+    if (error.response) {
+      const { status, data } = error.response;
+      console.error("Error response:", status, data);
+    } else if (error.request) {
+      console.error("No response received:", error.request);
+    } else {
+      console.error("Error setting up request:", error.message);
+    }
+
+    throw error;
+  }
+};
+
