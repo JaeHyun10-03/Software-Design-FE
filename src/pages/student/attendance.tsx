67ae06e28545551cd174ebe188353eb03cca5a30
LimJaeHyun <guswogus81@gmail.com>
2025-05-27 16:36:08 +0900

    ✨ feat: 학생페이지 - 학적, 성적 완료

--- src/pages/student/attendance.tsx
+++ src/pages/student/attendance.tsx
@@ -1,5 +1,12 @@
+import { StudentHeader } from "@/components/shared/StudentHeader";
+import { ReactNode } from "react";
+
 const attendance = () => {
-  return <div>attendance</div>;
+  return <div className="sm:m-0 m-4"></div>;
 };
 
 export default attendance;
+
+attendance.getLayout = (page: ReactNode) => {
+  return <StudentHeader>{page}</StudentHeader>;
+};
--- src/pages/student/behavior.tsx
+++ src/pages/student/behavior.tsx
@@ -1,4 +1,12 @@
+import { StudentHeader } from "@/components/shared/StudentHeader";
+import { ReactNode } from "react";
+
 const behavior = () => {
-  return <div>behavior</div>;
+  return <div className="sm:m-0 m-4"></div>;
 };
+
 export default behavior;
+
+behavior.getLayout = (page: ReactNode) => {
+  return <StudentHeader>{page}</StudentHeader>;
+};
