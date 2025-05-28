e2b29a3831e72d946f4cb99e73e0f8be1020be09
JaeHyun10-03 <guswogus81@gmail.com>
2025-05-28 03:16:25 +0000

    🐛 fix: 머지 후 에러 수정

--- src/components/behavior/StudentBehaviorContent.tsx
+++ src/components/behavior/StudentBehaviorContent.tsx
@@ -15,7 +15,7 @@ const BehaviorContent = () => {
     const getBehavior = async () => {
       try {
         const res = await GetBehavior(year,  Number(grade),  Number(classNumber), Number(studentId));
-        const data = res.data.response;
+        const data = res;
         setBehavior(data.behavior);
         setGeneralOpinion(data.generalComment);
         console.log("행동 데이터:", data);
@@ -28,7 +28,7 @@ const BehaviorContent = () => {
 
  
   return (
-    <div className="w-full h-full border border-[#a9a9a9] p-4">
+    <div className="w-full h-full scroll-auto p-4 pt-0 pb-0">
       <div className="flex flex-col gap-2">
         <p className="text-xl">행동특성</p>
         <textarea
@@ -37,12 +37,12 @@ const BehaviorContent = () => {
           className="min-h-48 border border-[#a9a9a9] rounded-md text-[#333333] text-base resize-none p-4 focus:outline-none"
         />
       </div>
-      <div className="flex flex-col gap-2 mt-16">
+      <div className="flex flex-col gap-2 mt-14">
         <p className="text-xl">종합의견</p>
         <textarea
           readOnly
           value={generalOpinion}
-          className="min-h-48 border border-[#a9a9a9] rounded-md text-[#333333] text-base resize-none p-4 focus:outline-none"
+          className="min-h-48 border border-[#a9a9a9] rounded-md text-[#333333] text-base resize-none p-4 pb-0 focus:outline-none"
         />
       </div>
     </div>
