3efcbf2e7e208d2e2ac9dd898ca80fd425e69144
JaeHyun10-03 <guswogus81@gmail.com>
2025-05-28 01:41:41 +0000

    ⬆️ update : GET API 연결 완료 & PUT, POST API 문제 발생

--- src/pages/behavior/index.tsx
+++ src/pages/behavior/index.tsx
@@ -1,4 +1,5 @@
 import BehaviorContent from "@/components/behavior/BehaviorContent";
+import DateFilter from "@/components/shared/DateFilter";
 import { Header } from "@/components/shared/Header";
 import StudentFilter from "@/components/shared/StudentFilter";
 import StudentList from "@/components/shared/StudentList";
@@ -9,6 +10,7 @@ export default function BehaviorPage() {
     <div className="mt-4 mb-8 h-[calc(100vh-120px)] flex flex-col mx-4 sm:mx-8">
       <div className="flex h-8">
         <StudentFilter />
+        <DateFilter />
       </div>
       <div className="flex flex-row gap-8 mt-4 flex-1">
         <div className="hidden sm:flex">
